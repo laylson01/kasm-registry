0a9dcb7e40e9557be115f4fc3ae6e0f0c13cd95d
Laylson C. Silva <laylson01@hotmail.com>
2024-06-11 07:35:57 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -1,7 +1,7 @@
 const nextConfig = {
   env: {
-    name: 'LAYLSON DA C. Silva ',
-    description: 'W O R K S P A C E R E G I S T R Y',
+    name: 'Containers Technologies ',
+    description: 'WORKSPACE REGISTRY',
     icon: '/img/logo.svg',
     listUrl: 'https://laylson01.github.io/kasm-registry/1.0/',
     contactUrl: 'https://lacolyn.com/',
